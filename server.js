472bab75426ff8ab898fc7378b2eec0ae1bd82cc
FactOK <147402799+FactNJS1014@users.noreply.github.com>
2025-09-16 16:23:47 +0700

    crud productTypes

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.post('/api/user/signIn', userController.signIn)
 
 //productType
 app.post('/api/productType/create', productType.create)
+app.get('/api/productType/list', productType.list)
+app.put('/api/productType/update/:id', productType.update)
+app.delete('/api/productType/remove/:id', productType.remove)
 
 app.listen(3001, '0.0.0.0' , () =>{
     console.log("Server running on port 3001")
